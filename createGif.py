e3b5ae841ea6d53405724aba490b3a0199700eb6
Rob Sletten <rsletten@gmail.com>
2021-02-03 13:45:59 -0500

    updated

--- createGif.py
+++ createGif.py
@@ -3,8 +3,13 @@ import os, sys
 
 path = "screenshots/"
 
-filenames = os.listdir(path)
-filenames.sort()
+def get_files(path):
+    filenames = os.listdir(path)
+    filenames.sort()
+    return filenames
+
+filenames = get_files(path)
+
 with imageio.get_writer('covid.gif', mode='I', duration = 0.15) as writer:
     for filename in filenames:
         image = imageio.imread("screenshots/%s" % filename)
